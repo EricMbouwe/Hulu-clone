45485625b103a624610f908d8d5bc6e9b49c5da7
ericmbouwe <ericmbouwe@yahoo.fr>
2021-05-04 23:17:27 +0100

    add flex and grid to result component

--- components/Result.js
+++ components/Result.js
@@ -1,13 +1,13 @@
-import Thumbnail from "./Thumbnail"
+import Thumbnail from './Thumbnail';
 
 function Result({ results }) {
   return (
-    <div>
-      {results?.map(result => (
+    <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+      {results?.map((result) => (
         <Thumbnail key={results.id} result={result} />
       ))}
     </div>
-  )
+  );
 }
 
-export default Result
+export default Result;
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,10 +1,11 @@
+import { ThumbUpIcon } from '@heroicons/react/outline';
 import Image from 'next/image';
 
 function Thumbnail({ result }) {
   const BASE_URL = 'https://image.tmdb.org/t/p/original';
 
   return (
-    <div>
+    <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:scale-105 hover:z-50">
       <Image
         layout="responsive"
         src={
@@ -14,7 +15,15 @@ function Thumbnail({ result }) {
       />
       <div className="p-2">
         <p className="truncate max-w-md">{result.overview}</p>
-        <h2>{result.title || result.original_name}</h2>
+        <h2 className="mt-1 text-white text-2xl transition-all duration-100 ease-in-out group-hover:font-bold">
+          {result.title || result.original_name}
+        </h2>
+        <p className="flex items-center opacity-0 group-hover:opacity-100">
+          {result.media_type && `${result.media_type} .`}{' '}
+          {result.release_date || result.first_air_date}.{' '}
+          <ThumbUpIcon className="h-5 mx-2" />
+          {result.vote_count}
+        </p>
       </div>
     </div>
   );
