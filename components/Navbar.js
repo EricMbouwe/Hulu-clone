e7e5ff434d42200f379d74379e8320928ff2dbe4
ericmbouwe <ericmbouwe@yahoo.fr>
2021-05-04 18:32:47 +0100

    add local env file

---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav>
+      
+    </nav>
+  )
+}
+
+export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       {/**header*/}
       <Header />
       {/**navbar*/}
+      <Navbar />
       {/**content*/}
     </div>
   )
