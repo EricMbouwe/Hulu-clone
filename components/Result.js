b7f1bd1eaa493c08f3c38678de8dfd927b3dab07
ericmbouwe <ericmbouwe@yahoo.fr>
2021-05-04 23:30:20 +0100

    add forwardref to thumbnail comp

--- components/Result.js
+++ components/Result.js
@@ -1,12 +1,13 @@
+import FlipMove from 'react-flip-move';
 import Thumbnail from './Thumbnail';
 
 function Result({ results }) {
   return (
-    <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
       {results?.map((result) => (
         <Thumbnail key={results.id} result={result} />
       ))}
-    </div>
+    </FlipMove>
   );
 }
 
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,11 +1,12 @@
 import { ThumbUpIcon } from '@heroicons/react/outline';
 import Image from 'next/image';
+import { forwardRef } from 'react';
 
-function Thumbnail({ result }) {
+const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = 'https://image.tmdb.org/t/p/original';
 
   return (
-    <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:scale-105 hover:z-50">
+    <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:scale-105 hover:z-50">
       <Image
         layout="responsive"
         src={
@@ -27,6 +28,6 @@ function Thumbnail({ result }) {
       </div>
     </div>
   );
-}
+});
 
 export default Thumbnail;
