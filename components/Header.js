b8e32de80482ef00c8268f695b60deb362ec29c6
ericmbouwe <ericmbouwe@yahoo.fr>
2021-05-04 13:53:32 +0100

    add styles to header

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,19 @@
+import { BadgeCheckIcon, CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon, UserIcon } from '@heroicons/react/outline'
 import Image from 'next/image';
 import HeaderIcon from './HeaderIcon';
 
 function Header() {
   return (
-    <header>
-      <h1>This is the header</h1>
-      <div>
-        <HeaderIcon title={'Home'} icon={'icon'} />
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderIcon title={'HOME'} Icon={HomeIcon} />
+        <HeaderIcon title={'TRENDINGS'} Icon={LightningBoltIcon} />
+        <HeaderIcon title={'VERIFIED'} Icon={BadgeCheckIcon} />
+        <HeaderIcon title={'COLLECTIONS'} Icon={CollectionIcon} />
+        <HeaderIcon title={'SEARCH'} Icon={SearchIcon} />
+        <HeaderIcon title={'ACCOUNT'} Icon={UserIcon} />
       </div>
+
       <Image
         src="https://links.papareact.com/ua6"
         className="object-contain"
